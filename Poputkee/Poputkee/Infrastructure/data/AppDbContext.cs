377b3a298db1a3e7909f7e78115ec507e7341bd3
Andrei Blizuchenko <blizu4enko.andrei@yandex.ru>
2025-05-04 18:36:20 +0300

    Изменение README.MD;Добавление директорий;Создание 2 файлов AppDbContext.cs и IRepository.cs

---
+++ Poputkee/Poputkee/Infrastructure/data/AppDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<User> Users { get; set; }
+    public DbSet<Trip> Trips { get; set; }
+    public DbSet<Booking> Bookings { get; set; }
+
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql("Host=localegost;Database=poputkee;Username=postgres;Password=1234");
+    }
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Booking>()
+            .HasKey(b => b.Id);
+
+        modelBuilder.Entity<Booking>()
+            .HasOne(b => b.Trip)
+            .WithMany(t => t.Bookings)
+            .HasForeignKey(b => b.TripId);
+
+        modelBuilder.Entity<Booking>()
+            .HasOne(b => b.Passenger)
+            .WithMany(u => u.Booking)
+            .HasForeignKey(b =>b.PassengerId);
+    }
+}
